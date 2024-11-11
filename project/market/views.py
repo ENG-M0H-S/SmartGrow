23a5b6c602b70196587f17bf3a96da9125bc4078
ENG-M0H-S <engmoh7511@gmail.com>
2024-11-11 07:38:26 +0300

    last update

--- project/market/views.py
+++ project/market/views.py
@@ -3,8 +3,9 @@ from .models import Product
 
 # Create your views here.
 def ProductsView(request):
-    products = Product.objects.all()
-    return render(request, 'market/products.html', {'products':products})
+    products = Product.objects.all().exclude(product_state=False)
+    x= {'frutes':products.filter(category_id=2)}
+    return render(request, 'market/products.html', x)
 
 def DetailsView(request):
     return render(request, 'market/details.html')
--- project/plants/models.py
+++ project/plants/models.py
@@ -3,7 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Categories(models.Model):
-    category_name = models.CharField(max_length=100)
+
+    TYPE_CHOICES = [
+            ('frute', 'الفواكه'),
+            ('vegetable', 'الخضار'),
+            ('grain', 'الحبوب'),
+            ('herbs', 'الاعشاب'),
+            ('ornamental', 'الزينة'),
+        ]   
+    category_name = models.CharField(max_length=100, choices=TYPE_CHOICES)
 
     def __str__ (self):
         return self.category_name
@@ -16,6 +24,9 @@ class Plants(models.Model):
     harvest = models.IntegerField()
     informations = models.TextField()
     image = models.ImageField(upload_to='photo/%y/%m/%d')
+    img1 = models.ImageField(upload_to='photo/%y/%m/%d')
+    img2 = models.ImageField(upload_to='photo/%y/%m/%d')
+    img3 = models.ImageField(upload_to='photo/%y/%m/%d')
 
     def __str__ (self):
         return self.plant_name
